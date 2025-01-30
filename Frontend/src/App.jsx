315efae3c485501885e31e39a27c0ff5e0882ced
ShashankVenkatesh <shashankvenkatesh7906@gmail.com>
2025-01-30 17:23:31 +0530

    M5 & M6

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable no-unused-vars */
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
-import {Login} from './Routes/Routes'
-
-const App = () => {
+import './App.css';
+import React from 'react';
+import {Login,Signup} from "./Routes/Routes"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/login' element={<Login/>}/>
+    <Route path='/Login' element={<Login/>}/>
+    <Route path='/Signup' element={<Signup/>}/>
     </Routes>
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/Routes/Routes.jsx
+++ Frontend/src/Routes/Routes.jsx
@@ -1,2 +1,4 @@
-import Login from "../Pages/Login"
-export{Login,}
+
+import Login from "../Pages/Login.jsx";
+import Signup from "../Pages/Signup.jsx";
+export { Login,Signup}
