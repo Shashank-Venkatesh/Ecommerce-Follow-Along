39d4ddc04087f3b0a36a179eeaeb689fe845bab3
ShashankVenkatesh <shashankvenkatesh7906@gmail.com>
2025-02-14 09:52:26 +0530

    updated

--- Frontend/src/Routes/Routes.jsx
+++ Frontend/src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import Login from "../pages/Login.jsx";
-import Signup from "../pages/Signup.jsx";
+import Signup from "../Pages/Signup.jsx";
 import Home from "../Pages/Home.jsx";
+import MyProducts from "../Pages/myProducts.jsx"
 import CreateProduct from "../Pages/CreateProduct.jsx"; // Check the correct path
 
-export { Login, Signup, Home, CreateProduct };
+export { Login, Signup, Home, CreateProduct, MyProducts };
